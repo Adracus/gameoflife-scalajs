173e2e00df3f6e79d59e3e1d81525fc26a66691c
Adracus <adracus@gmail.com>
2015-04-15 12:50:06 +0200

    Extract rule engine

--- src/main/scala/gameoflife/gamefield/GameField.scala
+++ src/main/scala/gameoflife/gamefield/GameField.scala
@@ -1,40 +1,47 @@
 package gameoflife.gamefield
 
+import gameoflife.core.{DefaultRuleEngine, RuleEngine}
+
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
 
 object GameField {
-  def random(x: Int, y: Int): GameField = {
+  def random(x: Int, y: Int, ruleEngine: RuleEngine = new DefaultRuleEngine): GameField = {
     val random = new Random()
     val fields = Array.ofDim[Boolean](x, y)
     for (x <- 0 until x; y <- 0 until y) {
       fields(x)(y) = random.nextBoolean()
     }
-    new GameField(fields)
+    new GameField(fields, ruleEngine)
   }
 }
 
 /**
  * Created by axel on 13/04/15.
  */
-class GameField (private val field: Array[Array[Boolean]]) {
+class GameField(private val field: Array[Array[Boolean]],
+                private val ruleEngine: RuleEngine = new DefaultRuleEngine()) {
   def width = field.length
+
   def height = if (0 == field.length) 0 else field(0).length
 
   def apply(x: Int, y: Int) = field(x)(y)
+
   def get(x: Int, y: Int) = apply(x, y)
 
   def next(): GameField = {
     val result = Array.ofDim[Boolean](width, height)
     for (x <- 0 until width; y <- 0 until height) {
-      livingNeighbours(x, y) match {
-        case 3 => result(x)(y) = true
-        case 2 => result(x)(y) = get(x, y)
-        case _ => false
-      }
+      result(x)(y) = ruleEngine.nextState(get(x, y), livingNeighbours(x, y))
     }
     new GameField(result)
+  }
+
+  def inRange(x: Int, y: Int) = x >= 0 && x < width && y >= 0 && y < height
+
+  def hasNext: Boolean = {
+    !isEverythingDead
   }
 
   private def isEverythingDead: Boolean = {
@@ -57,11 +64,5 @@ class GameField (private val field: Array[Array[Boolean]]) {
     if (inRange(x, y + 1)) values.append(get(x, y + 1))
     if (inRange(x - 1, y + 1)) values.append(get(x - 1, y + 1))
     values.toList
-  }
-
-  def inRange(x: Int, y: Int) = x >= 0 && x < width && y >= 0 && y < height
-
-  def hasNext: Boolean = {
-    !isEverythingDead
   }
 }
